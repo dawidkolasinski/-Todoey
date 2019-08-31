d37bada3048aeb5d891fe1aee8d1cd1ff09bbcc9
Dawid Kolasiński <dawid.m.kolasinski@gmail.com>
2019-08-31 16:21:06 +0200

    Convert to MVC design pattern

---
+++ Todoey/DataModel/Item.swift
@@ -0,0 +1,15 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Dawid Kolasinski on 31/08/2019.
+//  Copyright © 2019 Dawid Kolasinski. All rights reserved.
+//
+
+import Foundation
+
+
+class Item {
+    var title: String = ""
+    var done: Bool = false
+}
